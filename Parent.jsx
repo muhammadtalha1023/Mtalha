eda6e931f7433d118b53a6df4ed311b6fa5afa02
Muhammad Talha <116357645+muhammadtalha1023@users.noreply.github.com>
2022-11-02 21:25:38 +0500

    Add files via upload

---
+++ Parent.jsx
@@ -0,0 +1,36 @@
+import { Container, Row } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaVimeoSquare } from "react-icons/fa";
+import AutoLayoutExample from "./Child"
+import ChildA from "./ChildA";
+import ChildB from './ChildB';
+
+function Parent()
+{
+    return(
+      <div>
+        <div class="row">
+       <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home" >Free Shipping over $100 & 60 days returns</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <FaFacebook size="25" fontStyle="italic" title="facebook"/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <FaTwitter size="25" title="twitter"/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <FaVimeoSquare size="25" title="vimeo"/>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <AutoLayoutExample />
+    </div>
+      </div>
+    )
+}
+
+export default Parent;
